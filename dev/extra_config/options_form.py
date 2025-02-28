a53d14ad82a5ff77b0cfb1c79716ad0831c2c15f
Sarah <sarahmburbach@gmail.com>
2025-02-28 14:24:54 -0800

    make options form & templates applicable to all our setups (gpu-only and cpu-gpu clusters)

--- dev/extra_config/options_form.py
+++ dev/extra_config/options_form.py
@@ -1,8 +1,8 @@
 # modified from https://github.com/neurohackademy/nh2020-jupyterhub/blob/8bc0d8304d5090d3a1e21a038a68f0a940e31809/deployments/hub-neurohackademy-org/config/prod.yaml
 # ref for profile_options: https://jupyterhub-kubespawner.readthedocs.io/en/latest/spawner.html
 
-def _define_images(CONFIG):
-  """Define the image choices, based on CONFIG"""
+def _define_images(images):
+  """Define the image choices"""
   return {
     "Image": {
       "display_name": "Image",
@@ -11,7 +11,7 @@ def _define_images(CONFIG):
               "display_name": name,
               "default": (name == "datascience"),
               "kubespawner_override": {"image": url},
-          } for name, url in CONFIG["images"].items()
+          } for name, url in images.items()
       },
       "unlisted_choice": {
         "enabled": True,
@@ -27,8 +27,8 @@ def _define_images(CONFIG):
     }
   }
     
-def _define_num_gpus(CONFIG):
-  """Define choices for number of gpus, based on CONFIG"""
+def _define_num_gpus(gpu_counts):
+  """Define choices for number of gpus"""
   return {
     "GPUNum": {
       "display_name": "Number of GPUs",
@@ -40,12 +40,12 @@ def _define_num_gpus(CONFIG):
             "extra_resource_limits": {"nvidia.com/gpu": i},
             "extra_pod_config": {"runtimeClassName": "nvidia"},
           } if i > 0 else {},
-        } for i in CONFIG["gpu_counts"]
+        } for i in gpu_counts
       },
     }
   }
     
-def _define_gpu_nodes(CONFIG):
+def _define_gpu_nodes(node_info):
   """Define node choices, based on CONFIG"""
   return {
     "Node": {
@@ -57,56 +57,77 @@ def _define_gpu_nodes(CONFIG):
               "node_selector": {"kubernetes.io/hostname": str(node["node"])},
           },
         }
-        for node in CONFIG["node_info"] 
+        for node in node_info
       },
     }
   }
 
+# image descriptions
+image_des = """Choose your image.
+                <ul>
+                <li><b>datascience</b>: default data analysis environment (including interpreters for Python and R)</li>
+                <li><b>deeplearning</b>: extends datascience environment to include ML libraries (including torch, deepspeed, and 🤗)</li>
+                </ul>
+            """
+
+# define CPU profile
+def cpu_profile(CONFIG):
+  return [{
+    "display_name": "CPU Server",
+    "description": image_des,
+    "profile_options": {
+      **_define_images(CONFIG["images"]),
+    },
+    "kubespawner_override": {
+      "node_selector": {"node_profile": "cpu"},
+    }
+  }]
+
+# define GPU profile
+def gpu_profile(CONFIG):
+  return [{
+    "display_name": "GPU Server",
+    "description": image_des + "Reference the GPU availability below to select your node and number of GPUs.",
+    "profile_options": {
+      **_define_images(CONFIG["images"]),
+      **_define_gpu_nodes(CONFIG["node_info"]),
+      **_define_num_gpus(CONFIG["gpu_counts"]),
+    },
+  }]
+
+# define dev profile
+# def dev_profile(CONFIG):
+#   return [{
+#     "display_name": "Dev (admin only)",
+#     "description": "Set custom images, resources, etc for testing",
+#     "profile_options": {
+#       **_define_images(CONFIG),
+#       **_define_gpu_nodes(CONFIG),
+#       **_define_num_gpus(CONFIG),
+#     },
+#   }]
+
+# return profile options based on the server type and config provided 
 def dynamic_options_form_withconfig(CONFIG):
   def dynamic_options_form(self):
     
-    # For default server -> CPU profiles
-    if not self.name:
-      self.profile_list = [
-        {
-          "display_name": "CPU Server",
-          "description": (
-            "Choose your image."
-            "<ul>"
-            "<li><b>datascience</b>: default data analysis environment (including interpreters for Python and R)</li>"
-            "<li><b>deeplearning</b>: extends datascience environment to include ML libraries (including torch, deepspeed, and 🤗)</li>"
-            "</ul>"
-          ),
-          "profile_options": {
-            **_define_images(CONFIG),
-          },
-          "kubespawner_override": {
-            "node_selector": {"node_profile": "cpu"},
-          }
-        }
-      ]
+    server_type = CONFIG['server_type']
     
-    # For named server(s) -> GPU profiles
+    if server_type == 'cpu-gpu': # For default server -> CPU profiles, For named server -> GPU profiles
+      self.profile_list = cpu_profile(CONFIG) if not self.name else gpu_profile(CONFIG)
+    
+    elif server_type == 'cpu-only':
+      self.profile_list = cpu_profile(CONFIG)
+    
+    elif server_type == 'gpu-only':  
+      self.profile_list = gpu_profile(CONFIG)
+    
     else:
-      # add inference gpus for all users on named servers
-      self.profile_list = [
-        {
-          "display_name": "GPU Server",
-          "description": (
-            "Choose your image."
-            "<ul>"
-            "<li><b>datascience</b>: default data analysis environment (including interpreters for Python and R)</li>"
-            "<li><b>deeplearning</b>: extends datascience environment to include ML libraries (including torch, deepspeed, and 🤗)</li>"
-            "</ul>"
-            "Reference the GPU availability below to select your node and number of GPUs."
-          ),
-          "profile_options": {
-            **_define_images(CONFIG),
-            **_define_gpu_nodes(CONFIG),
-            **_define_num_gpus(CONFIG),
-          },
-        }
-      ]
+      raise Exception("Fix server type to be one of: 'cpu-gpu', 'cpu-only', or 'gpu-only'")
+    
+    # Dev profile for admins only
+    # if self.user.admin:
+    #   self.profile_list.extend(dev_profile())
 
     # Let KubeSpawner inspect profile_list and decide what to return.
     # ref: https://github.com/jupyterhub/kubespawner/blob/37a80abb0a6c826e5c118a068fa1cf2725738038/kubespawner/spawner.py#L1885-L1935
